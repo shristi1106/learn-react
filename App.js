020fccb69b8540d8e80ee0c892449b523b1cd885
Shristi Verma <shristiverma1311@gmail.com>
2024-01-04 22:09:10 +0530

    part-01

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", {id:"heading"}, "I am shri")//{} this obj is a place where u will give attributes to ur tag
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading);
